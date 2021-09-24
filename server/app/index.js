a04d81edd9fda44cdda806cd50bc6ab50736af98
Manav Misra <manavm@visionwebsoft.com>
2021-09-24 06:46:30 -0500

    Initial commit

---
+++ server/app/index.js
@@ -0,0 +1,27 @@
+// Entry point for the application
+import express from "express";
+import pino from "express-pino-logger";
+import config from "./config.js";
+// TODO: Import the routes
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Hello World");
+});
+
+// Logging middleware
+app.use(
+  pino({
+    prettyPrint: { colorize: true, levelFirst: true },
+    messageFormat: "{levelLabel} - {pid} - url:{request.url}",
+  })
+);
+
+// TODO: Use json middleware (if needed)
+
+// TODO: Mount the routes (maybe 🤔 /api)
+
+app.listen(config.port, () => {
+  console.log(`Server 🏃🏾‍♂️ at: http://localhost:${config.port}`);
+});
