2ec8cdece75c434f3a5fe9361ed6621eedad32d0
Manav Misra <manavm@visionwebsoft.com>
2021-09-24 19:02:02 -0500

    Add Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./views/Home/HomePage";
+import MatchupPage from "./views/Matchup/MatchupPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <main className="bg-dark-cyan-blue w-screen min-h-screen flex items-center justify-center">
-      <HomePage />
+      <Router>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/matchup" component={MatchupPage} />
+      </Router>
     </main>
   );
 }
--- client/src/views/Home/HomePage.jsx
+++ client/src/views/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import api from "@app/services";
 import { Container, Section } from "components/Card";
 import HR from "components/HR";
 import React from "react";
+import { Link } from "react-router-dom";
 // We can name this whatever as it is `export default`
 import List from "./MatchupList";
 
@@ -23,12 +24,12 @@ function HomePage() {
       </Section>
       <Section heading="Ready to create a new matchup?">
         <div className="flex my-2 justify-center">
-          <a
-            href="#"
+          <Link
+            to="/matchup"
             className="capitalize bg-red-500 text-xl text-white rounded-md px-6 py-2"
           >
             Create matchup!
-          </a>
+          </Link>
         </div>
       </Section>
     </Container>
