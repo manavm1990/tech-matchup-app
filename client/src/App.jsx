6973645df5526c2d6e0d04b37cbd7a19973a2d2d
Manav Misra <manavm@visionwebsoft.com>
2021-09-25 00:07:17 -0500

    :construction: Provide Apollo to Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,26 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./views/Home/HomePage";
 import MatchupPage from "./views/Matchup/MatchupPage";
 import Vote from "./views/Vote/VotePage";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <main className="bg-dark-cyan-blue w-screen min-h-screen flex items-center justify-center">
-      <Router>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/matchup" component={MatchupPage} />
-        <Route exact path="/matchup/:id" component={Vote} />
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/matchup" component={MatchupPage} />
+          <Route exact path="/matchup/:id" component={Vote} />
+        </Router>
+      </ApolloProvider>
     </main>
   );
 }
